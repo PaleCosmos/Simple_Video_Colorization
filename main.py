e950b77ccdd07daef3af671cd2a5e19e4620e5f0
PaleCosmos <tkdgusdlqhek@hanmail.net>
2019-09-13 16:44:12 +0900

    20190913/4:44PM

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import cv2
 
-
 print(cv2.__version__)
 
-image = cv2.imread('resources/image/bitmex.png', cv2.IMREAD_COLOR)
-cv2.imshow("bitmex", image)
-cv2.waitKey(0)
+directory = 'resources/sample/sample.mp4'
+
+capture = cv2.VideoCapture(directory)
+
+while True:
+    if capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT):
+        capture.open(directory)
+
+    ret, frame = capture.read()
+    cv2.imshow("VideoFrame", frame)
+
+    if cv2.waitKey(33) > 0: break
+
+capture.release()
 cv2.destroyAllWindows()
