7a0fba781a2d73289c041184318df5f5bc0b5a9e
Sanghyeon Park <tkdgusdlqhek@hanmail.net>
2019-11-06 18:52:39 +0900

    신기방기

--- objectDetecting/objectDetect.py
+++ objectDetecting/objectDetect.py
@@ -1,121 +1,131 @@
-# import the necessary packages
-from collections import deque
-from imutils.video import VideoStream
 import numpy as np
-import argparse
 import cv2
-import imutils
-import time
 
-directory = '../resources/sample/'
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-                help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-                help="max buffer size")
-args = vars(ap.parse_args())
+col, width, row, height = -1, -1, -1, -1
+frame = None
+frame2 = None
+inputMode = False
+rectangle = False
+trackWindow = None
+roi_hist = None
+roi = None
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
-pts = deque(maxlen=args["buffer"])
+boundaries = [
+    ([200, 200, 200], [255, 255, 255]),
+    ([86, 31, 4], [220, 88, 50]),
+    ([25, 146, 190], [62, 174, 250]),
+    ([103, 86, 65], [145, 133, 128])
+]
 
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-    vs = VideoStream(src=0).start()
 
-# otherwise, grab a reference to the video file
-else:
-    vs = cv2.VideoCapture(0)
+def onMouse(event, x, y, flags, param):
+    global col, width, row, height, frame, frame2, inputMode
+    global rectangle, roi_hist, trackWindow
 
-# allow the camera or video file to warm up
-time.sleep(2.0)
+    if inputMode:
+        if event == cv2.EVENT_LBUTTONDOWN:
+            rectangle = True
+            col, row = x, y
 
-# keep looping
-while True:
-    # grab the current frame
-    frame = vs.read()
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if rectangle:
+                frame = frame2.copy()
+                cv2.rectangle(frame, (col, row), (x, y), (0, 255, 0), 2)
+                cv2.imshow('frame', frame)
 
-    # handle the frame from VideoCapture or VideoStream
-    frame = frame[1] if args.get("video", False) else frame
+        elif event == cv2.EVENT_LBUTTONUP:
+            inputMode = False
+            rectangle = False
+            cv2.rectangle(frame, (col, row), (x, y), (0, 255, 0), 2)
+            height, width = abs(row - y), abs(col - x)
+            trackWindow = (col, row, width, height)
+            roi = frame[row:row + height, col:col + width]
+            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+            roi_hist = cv2.calcHist([roi], [0], None, [180], [0, 180])
+            cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+    return
 
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
-    if frame is None:
-        break
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    frame = imutils.resize(frame, width=600)
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+def camShift():
+    global frame, frame2, inputMode, trackWindow, roi_hist, roi, boundaries
 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+    try:
+        cap = cv2.VideoCapture('../resources/sample/sample.mp4')
+        cap.set(3, 480)
+        cap.set(4, 320)
+    except:
+        print('Cam Failed')
+        return
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-    center = None
+    ret, frame = cap.read()
 
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    cv2.namedWindow('frame')
+    cv2.setMouseCallback('frame', onMouse, param=(frame, frame2))
 
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-                       (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+    termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
 
-    # update the points queue
-    pts.appendleft(center)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # loop over the set of tracked points
-    for i in range(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
-        if pts[i - 1] is None or pts[i] is None:
-            continue
+        lower, upper = boundaries[0]
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
 
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        h, w = frame.shape[:2]
 
-    # show the frame to our screen
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+        blank_image = np.zeros((h, w, 3), np.uint8)
+        blank_image[:] = (0, 0, 232)
 
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        break
+        blank_image2 = np.zeros((h, w, 3), np.uint8)
+        blank_image2[:] = (255, 255, 255)
+        blank_image3 = np.zeros((h, w, 3), np.uint8)
+        blank_image3[:] = (0, 0, 0)
 
-# if we are not using a video file, stop the camera video stream
-if not args.get("video", False):
-    vs.stop()
+        mask = cv2.inRange(frame, lower, upper)  # 색부분
 
-# otherwise, release the camera
-else:
-    vs.release()
+        trim = cv2.bitwise_or(frame, blank_image2, mask=mask)  # 몸통이하양
 
-# close all windows
-cv2.destroyAllWindows()
+        trim2 = cv2.bitwise_and(trim, blank_image, mask=mask)  # 몸통이파랑
+
+        trim3 = cv2.bitwise_and(frame, trim)
+
+        trim4 = cv2.bitwise_xor(trim3, frame)
+
+        frameNot = cv2.bitwise_and(frame, trim2)
+
+        frame = cv2.bitwise_or(trim4, trim2)
+
+        # frame = cv2.bitwise_xor(frame, trim2)
+
+        if trackWindow is not None:
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+            ret, trackWindow = cv2.CamShift(dst, trackWindow, termination)
+
+            pts = cv2.boxPoints(ret)
+            pts = np.int0(pts)
+
+            cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
+
+        cv2.imshow('frame', frame)
+
+        k = cv2.waitKey(100) & 0xFF
+        if k == 27:
+            break
+
+        if k == ord('i'):
+            print('추적한 영역을 지정하고 아무키나 누르세요')
+            inputMode = True
+            frame2 = frame.copy()
+
+            while inputMode:
+                cv2.imshow('frame', frame)
+                cv2.waitKey(0)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+camShift()
