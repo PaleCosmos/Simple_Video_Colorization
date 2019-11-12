2f3d8004493755d43c7263e243be7e32bfe4a29a
eom012 <33171438+eom012@users.noreply.github.com>
2019-11-12 15:29:27 +0900

    updates test.py and creates test_hsv.py
    test.py is using RGB, and the file is correct operation.
    test_hsv.py is uisng HSV, and the file is not correct operation

--- Server/objectDetecting/test_hsv.py
+++ Server/objectDetecting/test_hsv.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+# global variance
 col, width, row, height = -1, -1, -1, -1
 frame = None
 frame2 = None
@@ -20,6 +21,7 @@ boundaries = [
 
 
 def onMouse(event, x, y, flags, param):
+    # grab references to the global variables
     global col, width, row, height, frame, frame2, inputMode, blank_image3, s, h, w
     global rectangle, roi_hist, trackWindow
 
@@ -44,9 +46,11 @@ def onMouse(event, x, y, flags, param):
             roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
             roi_hist = cv2.calcHist([roi], [0], None, [180], [0, 180])
             cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+            # set blank_image
             blank_image3 = np.zeros((h, w, 3), np.uint8)
             blank_image3[:] = (0, 0, 0)
             blank_image3[row:row + height, col:col + width] = (255, 255, 255)
+            # switch on
             s = True
     return
 
@@ -67,8 +71,6 @@ def camShift():
 
     # read decode frame
     ret, frame = cap.read()
-
-    # set window name 'frame' 
     cv2.namedWindow('frame')
     # check mouse event, and callback
     cv2.setMouseCallback('frame', onMouse, param=(frame, frame2))
@@ -76,16 +78,16 @@ def camShift():
     # drive the meanShift back to 10 times or until the difference between C1_o and C1_r is 1pt.
     termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
 
-    # color lower, upper
-    lower, upper = boundaries[0]
-    lower = np.array(lower, dtype="uint8")
-    upper = np.array(upper, dtype="uint8")
-
     while True:
         # read decode frame
         ret, frame = cap.read()
         if not ret:
             break
+
+        # color lower, upper
+        lower, upper = boundaries[0]
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
 
         # frame height, width
         h, w = frame.shape[:2]
@@ -106,25 +108,16 @@ def camShift():
         # set the color range
         mask = cv2.inRange(frame, lower, upper)  # 색부분
         # in mask == mask, frame or bloack_image2 (set white)
-        trim1 = cv2.bitwise_or(frame, blank_image2, mask=mask)  # 몸통이하양
+        trim = cv2.bitwise_or(frame, blank_image2, mask=mask)  # 몸통이하양
         if s:
-            trim = cv2.bitwise_and(blank_image3, trim1)
-            # in mask == mask, frame or bloack_image (set color(blue))
-            trim2 = cv2.bitwise_and(trim, blank_image)  # 몸통이파랑 
-            # frame and trim; background image(not object)
-            trim3 = cv2.bitwise_and(frame, trim)
-            # trim3 xor frame; 
-            trim4 = cv2.bitwise_xor(trim3, frame)
-        else:
-            # in mask == mask, frame or bloack_image (set color(blue))
-            trim2 = cv2.bitwise_and(trim1, blank_image)  # 몸통이파랑
-            # cv2.imshow("btrim2", trim2)
-            # frame and trim; background image(not object)
-            trim3 = cv2.bitwise_and(frame, trim1)
-            # cv2.imshow("btrim3", trim3)
-            # trim3 xor frame; 
-            trim4 = cv2.bitwise_xor(trim3, frame)
-            # cv2.imshow("btrim4", trim4)
+            trim = cv2.bitwise_and(blank_image3, trim)
+        # in mask == mask, frame or bloack_image (set color(blue))
+        trim2 = cv2.bitwise_and(trim, blank_image)  # 몸통이파랑
+        # frame and trim; background image(not object)
+        trim3 = cv2.bitwise_and(frame, trim)
+        # trim3 xor frame;
+        trim4 = cv2.bitwise_xor(trim3, frame)
+
         frameNot = cv2.bitwise_and(frame, trim2)
 
         frame = cv2.bitwise_or(trim4, trim2)
@@ -135,12 +128,14 @@ def camShift():
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
             ret, trackWindow = cv2.CamShift(dst, trackWindow, termination)
+
             pts = cv2.boxPoints(ret)
             pts = np.int0(pts)
+
             # roi is white, other is black
             blank_image3[:] = (0, 0, 0)
             blank_image3[np.min(pts[:, 1]):np.max(pts[:, 1]), np.min(pts[:, 0]):np.max(pts[:, 0])] = (255, 255, 255)
-            cv2.imshow("box", blank_image3)
+
             cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
 
         cv2.imshow('frame', frame)
@@ -162,6 +157,4 @@ def camShift():
     cv2.destroyAllWindows()
 
 
-# main
-if __name__ == '__main__':
-    camShift()
+camShift()
