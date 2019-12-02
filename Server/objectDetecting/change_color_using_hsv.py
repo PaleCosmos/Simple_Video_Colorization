9369520cbd964591872b99af8cc92bae70593ba3
eom012 <33171438+eom012@users.noreply.github.com>
2019-12-02 14:03:59 +0900

    ??

--- Server/objectDetecting/change_color_using_hsv.py
+++ Server/objectDetecting/change_color_using_hsv.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-import colorsys
+import math
 
 col, width, row, height = -1, -1, -1, -1
 frame = None
@@ -21,14 +21,6 @@ hsv_boundaries_0 = [
 hsv_boundaries_1 = [
     ([170, 70, 50], [180, 255, 255])
 ]
-
-def calculate(a, b, c, x, y):
-    return abs(a * x + b * y + c) / math.sqrt(a ** 2 + b ** 2)
-
-
-def distance(a, b, c, d):
-    return math.sqrt((a - c) ** 2 + (b - d) ** 2)
-
 
 def onMouse(event, x, y, flags, param):
     global col, width, row, height, frame, frame2, inputMode, blank_image, s, h, w
@@ -112,27 +104,58 @@ def camShift():
         # 변경 색상
         ChangeColor = np.zeros((h, w, 3), np.uint8)
         ChangeColor[:] = (0, 255, 255)
+        #cv2.imshow("bgr", ChangeColor)
+        # 변경 색상을 HSV로 변환 후, H/S/V의 값으로 split
         HSV_ChangeColor = cv2.cvtColor(ChangeColor, cv2.COLOR_BGR2HSV)
+        cv2.imshow("change color(HSV)", HSV_ChangeColor)
         CC_h, CC_s, CC_v = cv2.split(HSV_ChangeColor)
 
         white_image = np.zeros((h, w, 3), np.uint8)
         white_image[:] = (255, 255, 255)
 
         # 선택된 색상 영역을 하얗게 만든다
-        # hsv 색상으로 색상 선택하기
+        # HSVframe에서 lower0 ~ upper0 영역을 white로 변환
         mask_range_0 = cv2.inRange(HSV_frame, lower0, upper0)
         mask_white_0 = cv2.bitwise_or(HSV_frame, white_image, mask=mask_range_0)
+        # HSVframe에서 lower1 ~ upper1 영역을 white로 변환
         mask_range_1 = cv2.inRange(HSV_frame, lower1, upper1)
         mask_white_1 = cv2.bitwise_or(HSV_frame, white_image, mask=mask_range_1)
+        # 두 영역을 모두 포함
         mask_white = cv2.bitwise_or(mask_white_0, mask_white_1)
+        #cv2.imshow('mask_white', mask_white)
+
+        white_px = np.asarray([255, 255, 255])
+        black_px = np.asarray([0, 0, 0])
+        #img_array = np.array(img)
         # 선택된 색상 영역과 선택된 영역을 하얗게 만든다
         if s:
+            # 선택된 영역(하얀색)은 하얗게, 나머지 배경은 검정색으로 변환
             mask_white = cv2.bitwise_and(blank_image, mask_white)
-
+            '''
+            for r in range(h):
+                for c in range(w):
+                    px = mask_white[r][c]
+                    if all(px == white_px):
+                        mask_white[r][c] = black_px
+                    if all(px == black_px):
+                        mask_white[r][c] = white_px
+            cv2.imshow('mask_black', mask_white)
+            frame = cv2.bitwise_or(frame, mask_white)
+            '''
+            #cv2.imshow('mask_white', mask_white)
+            mask_black = cv2.bitwise_not(mask_white)
+            #cv2.imshow('mask_black', mask_black)
+            frame = cv2.bitwise_and(frame, mask_black)
+            # 변환하고자 하는 색상의 H/S와 원래의 V(HSV_frame의 V)를 합친다
             merge_color = cv2.merge([CC_h, CC_s, F_v])
+            #cv2.imshow('merge', merge_color)
+            # 변환한 색상과 까만 배경
             merge_color = cv2.bitwise_and(merge_color, mask_white)
-            merge_color = cv2.cvtColor(merge_color, cv2.COLOR_HSV2BGR)
             
+            # 변환한 사진을 BGR로 변환
+            merge_color = cv2.cvtColor(merge_color, cv2.COLOR_HSV2BGR)
+            #cv2.imshow('merge_color', merge_color)
+            # 이를 frame과 합쳐, frame의 배경과 merge_color의 색상 변경 결과를 저장
             merge_result = cv2.bitwise_or(frame, merge_color)
             cv2.imshow('merge_result', merge_result)
 
